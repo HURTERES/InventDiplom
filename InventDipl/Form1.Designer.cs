e36c152c9456a1ae108dd2d70a80527ee581dd7b
Danya_Ls Danya_Ls <dany.levinsas@mail.ru>
2024-05-04 00:50:05 +0300

    Add project files.

---
+++ InventDipl/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace InventDipl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BtnClose = new Guna.UI2.WinForms.Guna2Button();
+            this.PanelLogin = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
+            this.TbxPass = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.guna2CustomGradientPanel1 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
+            this.guna2DragControl1 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.guna2Elipse2 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.guna2Elipse3 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.guna2Elipse4 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.guna2Elipse5 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.panel1.SuspendLayout();
+            this.PanelLogin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackgroundImage = global::InventDipl.Properties.Resources.Fon2;
+            this.panel1.Controls.Add(this.BtnClose);
+            this.panel1.Controls.Add(this.PanelLogin);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.guna2CustomGradientPanel1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1290, 636);
+            this.panel1.TabIndex = 0;
+            // 
+            // BtnClose
+            // 
+            this.BtnClose.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.BtnClose.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.BtnClose.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.BtnClose.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.BtnClose.FillColor = System.Drawing.Color.White;
+            this.BtnClose.Font = new System.Drawing.Font("Felix Titling", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnClose.ForeColor = System.Drawing.Color.Indigo;
+            this.BtnClose.Location = new System.Drawing.Point(1148, 13);
+            this.BtnClose.Name = "BtnClose";
+            this.BtnClose.Size = new System.Drawing.Size(127, 36);
+            this.BtnClose.TabIndex = 8;
+            this.BtnClose.Text = "Close";
+            this.BtnClose.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // PanelLogin
+            // 
+            this.PanelLogin.Controls.Add(this.TbxPass);
+            this.PanelLogin.Controls.Add(this.label1);
+            this.PanelLogin.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(163)))), ((int)(((byte)(249)))));
+            this.PanelLogin.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(163)))), ((int)(((byte)(249)))));
+            this.PanelLogin.Location = new System.Drawing.Point(95, 79);
+            this.PanelLogin.Name = "PanelLogin";
+            this.PanelLogin.Size = new System.Drawing.Size(514, 485);
+            this.PanelLogin.TabIndex = 6;
+            // 
+            // TbxPass
+            // 
+            this.TbxPass.BackColor = System.Drawing.Color.Transparent;
+            this.TbxPass.BorderColor = System.Drawing.Color.White;
+            this.TbxPass.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.TbxPass.DefaultText = "";
+            this.TbxPass.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.TbxPass.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.TbxPass.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.TbxPass.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.TbxPass.FocusedState.BorderColor = System.Drawing.Color.Transparent;
+            this.TbxPass.Font = new System.Drawing.Font("Felix Titling", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TbxPass.ForeColor = System.Drawing.Color.Indigo;
+            this.TbxPass.HoverState.BorderColor = System.Drawing.Color.White;
+            this.TbxPass.IconLeft = global::InventDipl.Properties.Resources.close_eye;
+            this.TbxPass.IconLeftOffset = new System.Drawing.Point(3, 0);
+            this.TbxPass.IconLeftSize = new System.Drawing.Size(24, 24);
+            this.TbxPass.Location = new System.Drawing.Point(49, 140);
+            this.TbxPass.Margin = new System.Windows.Forms.Padding(12, 13, 12, 13);
+            this.TbxPass.Name = "TbxPass";
+            this.TbxPass.PasswordChar = '\0';
+            this.TbxPass.PlaceholderText = "";
+            this.TbxPass.SelectedText = "";
+            this.TbxPass.Size = new System.Drawing.Size(421, 51);
+            this.TbxPass.TabIndex = 1;
+            this.TbxPass.TextOffset = new System.Drawing.Point(3, -2);
+            this.TbxPass.IconLeftClick += new System.EventHandler(this.TbxPass_IconLeftClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Felix Titling", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Indigo;
+            this.label1.Location = new System.Drawing.Point(154, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 57);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LOGIN";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::InventDipl.Properties.Resources.Man;
+            this.pictureBox1.Location = new System.Drawing.Point(553, 106);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(648, 435);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // guna2CustomGradientPanel1
+            // 
+            this.guna2CustomGradientPanel1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(163)))), ((int)(((byte)(249)))));
+            this.guna2CustomGradientPanel1.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(163)))), ((int)(((byte)(249)))));
+            this.guna2CustomGradientPanel1.Location = new System.Drawing.Point(80, 79);
+            this.guna2CustomGradientPanel1.Name = "guna2CustomGradientPanel1";
+            this.guna2CustomGradientPanel1.Size = new System.Drawing.Size(514, 485);
+            this.guna2CustomGradientPanel1.TabIndex = 5;
+            // 
+            // guna2DragControl1
+            // 
+            this.guna2DragControl1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2DragControl1.DragStartTransparencyValue = 1D;
+            this.guna2DragControl1.TargetControl = this;
+            this.guna2DragControl1.UseTransparentDrag = true;
+            // 
+            // guna2Elipse1
+            // 
+            this.guna2Elipse1.BorderRadius = 25;
+            this.guna2Elipse1.TargetControl = this.pictureBox1;
+            // 
+            // guna2Elipse2
+            // 
+            this.guna2Elipse2.BorderRadius = 10;
+            this.guna2Elipse2.TargetControl = this;
+            // 
+            // guna2Elipse3
+            // 
+            this.guna2Elipse3.BorderRadius = 25;
+            this.guna2Elipse3.TargetControl = this.guna2CustomGradientPanel1;
+            // 
+            // guna2Elipse4
+            // 
+            this.guna2Elipse4.BorderRadius = 15;
+            this.guna2Elipse4.TargetControl = this.TbxPass;
+            // 
+            // guna2Elipse5
+            // 
+            this.guna2Elipse5.BorderRadius = 15;
+            this.guna2Elipse5.TargetControl = this.BtnClose;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1314, 660);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.panel1.ResumeLayout(false);
+            this.PanelLogin.ResumeLayout(false);
+            this.PanelLogin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        public Guna.UI2.WinForms.Guna2CustomGradientPanel PanelLogin;
+        private Guna.UI2.WinForms.Guna2TextBox TbxPass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Guna.UI2.WinForms.Guna2CustomGradientPanel guna2CustomGradientPanel1;
+        private Guna.UI2.WinForms.Guna2Button BtnClose;
+        private Guna.UI2.WinForms.Guna2DragControl guna2DragControl1;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse2;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse3;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse4;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse5;
+    }
+}
+
---
+++ InventDipl/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventDipl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            TbxPass.UseSystemPasswordChar = true;
+        }
+
+        private void TbxPass_IconLeftClick(object sender, EventArgs e)
+        {
+            Image visibleIcon = Properties.Resources.visible;
+            Image closeEyeIcon = Properties.Resources.close_eye;
+
+            if (IsSameImage(TbxPass.IconLeft, visibleIcon))
+            {
+                TbxPass.IconLeft = closeEyeIcon;
+                TbxPass.UseSystemPasswordChar = true;
+            }
+            else if (IsSameImage(TbxPass.IconLeft, closeEyeIcon))
+            {
+                TbxPass.IconLeft = visibleIcon;
+                TbxPass.UseSystemPasswordChar = false;
+            }
+        }
+
+        private bool IsSameImage(Image img1, Image img2)
+        {
+            using (MemoryStream ms1 = new MemoryStream(), ms2 = new MemoryStream())
+            {
+                img1.Save(ms1, img1.RawFormat);
+                img2.Save(ms2, img2.RawFormat);
+                return ms1.ToArray().SequenceEqual(ms2.ToArray());
+            }
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
